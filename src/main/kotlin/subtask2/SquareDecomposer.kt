dd3fe0c34ffb294cf8981d469ab92a601beea8fe
VasiliB <73487624+VasiliB@users.noreply.github.com>
2022-03-14 23:05:55 +0300

    TelephoneFinder solved

--- src/main/kotlin/subtask2/SquareDecomposer.kt
+++ src/main/kotlin/subtask2/SquareDecomposer.kt
@@ -1,9 +1,119 @@
 package subtask2
 
-class SquareDecomposer {
+import kotlin.math.floor
+import kotlin.math.sqrt
 
+
+class SquareDecomposer {
+    // найти максимальный квадрат про должить операции с остатком, реверсировать массив
+    //подбирать сравнивая квадрат с квадратом числа, с разницей повторять
     // TODO: Complete the following function
     fun decomposeNumber(number: Int): Array<Int>? {
-        throw NotImplementedError("Not implemented")
+        val bufferList = mutableListOf<Int>()
+        var number2 = number * number
+        var element = number - 1
+//        var i = number
+        println(root(number2 - 1))
+        while (element >= 1) {
+            bufferList.add(element)
+            number2 -= element * element
+            element = root(number2)
+        }
+        var result = bufferList.reversed().toTypedArray()
+        println(bufferList.reversed())
+        // -1 для каждого элемента, прогон начиая с цикл с него него, сравнение суммы с кввадратом
+        if (bufferList.isNotEmpty() || number != 2) {
+            for (i in 0..bufferList.lastIndex) {
+                if (bufferList[i] > bufferList[i + 1]) {
+                } else {
+
+                    var y = 1
+                    element = bufferList[1]
+                    number2 = bufferList[0]*bufferList[0]
+
+                    while (element >= 1 || i<bufferList.lastIndex) {
+                        bufferList[y] = element-1
+                        println(bufferList.reversed())
+                        number2 -= element * element
+                        element = root(number2)
+//                i++
+                    }
+                    if (squareSum(bufferList) != number * number){
+                        y++
+                    }
+                }
+            }
+
+            result = bufferList.reversed().toTypedArray()
+        }
+
+//        if (bufferList.isNullOrEmpty() || number == 2) {
+//            return null} else {
+//            for (i in 0..bufferList.lastIndex) {
+//                if (bufferList[i] <= bufferList[i + 1]) {
+//                    return bufferList.reversed().toIntArray()
+//                } else return bufferList.reversed().toIntArray()
+//            }
+//        }
+
+
+        return if (bufferList.isNullOrEmpty() || number == 2) {
+            null
+        } else {
+            result
+        }
+    }
+
+    private fun root(number: Int): Int {
+        return floor(sqrt(number.toDouble())).toInt()
+    }
+
+    private fun squareSum(numbers: MutableList<Int>): Int {
+        var result = 0
+        for (i in 0..numbers.lastIndex) {
+            val res = numbers[i] * numbers[i]
+            result += res
+        }
+        return result
     }
 }
+
+//val result: Array<Int>?
+//var number2 = number * number
+//var startNumber = number2 - 1
+//var element = 0
+//var i = 0
+//var k = 0
+//var bufferList = mutableListOf<Int>()
+//while (i * i < startNumber) {
+////            println("startNumber=$startNumber")
+////            println("i=$i")
+//    k++
+////            println("k=$k")
+//    if (k * k >= startNumber) {
+//        element = i
+////                println("k=$k")
+////                println(element)
+//        i = 0
+//        k = 0
+//        bufferList.add(element)
+//        startNumber = (number2 - element * element) - 1
+//        number2 = startNumber
+////                println("startNumber=$startNumber")
+//    } else {
+//        i++
+//    }
+//}
+////        println(bufferList)
+////        startNumber = (number2 - element*element)-1
+////        i = 0
+////        while (i*i < startNumber) {
+////            element = i
+////            i++
+////        }
+////
+////        println(element)
+//result = bufferList.reversed().toTypedArray()
+//println(bufferList.reversed())
+//println(squareSum(bufferList))
+//println(number*number)
--- src/main/kotlin/subtask3/TelephoneFinder.kt
+++ src/main/kotlin/subtask3/TelephoneFinder.kt
@@ -4,6 +4,89 @@ class TelephoneFinder {
 
     // TODO: Complete the following function
     fun findAllNumbersFromGivenNumber(number: String): Array<String>? {
-        throw NotImplementedError("Not implemented")
+//val n0 = arrayListOf(8)
+        val n0: Array<Char> = arrayOf('8')
+        val n1: Array<Char> = arrayOf('2', '4')
+        val n2: Array<Char> = arrayOf('1', '3', '5')
+        val n3: Array<Char> = arrayOf('2', '6')
+        val n4: Array<Char> = arrayOf('1', '5', '7')
+        val n5: Array<Char> = arrayOf('2', '4', '6', '8')
+        val n6: Array<Char> = arrayOf('3', '5', '9')
+        val n7: Array<Char> = arrayOf('4', '8')
+        val n8: Array<Char> = arrayOf('5', '7', '9', '0')
+        val n9: Array<Char> = arrayOf('6', '8')
+        val bufferList = mutableListOf<String>()
+        for (i in 0..number.lastIndex)
+            when (number[i]) {
+                '0' -> for (n in 0..n0.lastIndex) {
+                    var buffer: String = number
+                    buffer =  buffer.replace(buffer[i], n0[n], true)
+                    bufferList.add(buffer)
+                }
+//                    bufferList.add(variator(number, n0, i, bufferList))
+
+                '1' -> for (n in 0..n1.lastIndex) {
+                    var buffer: String = number
+                    buffer =  buffer.replace(buffer[i], n1[n], true)
+                    bufferList.add(buffer)
+                }
+                '2' -> for (n in 0..n2.lastIndex) {
+                    var buffer: String = number
+                    buffer =  buffer.replace(buffer[i], n2[n], true)
+                    bufferList.add(buffer)
+                }
+                '3' -> for (n in 0..n3.lastIndex) {
+                    var buffer: String = number
+                    buffer =  buffer.replace(buffer[i], n3[n], true)
+                    bufferList.add(buffer)
+                }
+                '4' -> for (n in 0..n4.lastIndex) {
+                    var buffer: String = number
+                    buffer =  buffer.replace(buffer[i], n4[n], true)
+                    bufferList.add(buffer)
+                }
+                '5' -> for (n in 0..n5.lastIndex) {
+                    var buffer: String = number
+                  buffer =  buffer.replace(buffer[i], n5[n], true)
+//                    buffer[i] = n5[n]
+                    bufferList.add(buffer)
+                }
+                '6' -> for (n in 0..n6.lastIndex) {
+                    var buffer: String = number
+                    buffer =  buffer.replace(buffer[i], n6[n], true)
+                    bufferList.add(buffer)
+                }
+                '7' -> for (n in 0..n7.lastIndex) {
+                    var buffer: String = number
+                    buffer =  buffer.replace(buffer[i], n7[n], true)
+                    bufferList.add(buffer)
+                }
+                '8' -> for (n in 0..n8.lastIndex) {
+                    var buffer: String = number
+                    buffer =  buffer.replace(buffer[i], n8[n], true)
+                    bufferList.add(buffer)
+                }
+                '9' -> for (n in 0..n9.lastIndex) {
+                    var buffer: String = number
+                    buffer =  buffer.replace(buffer[i], n9[n], true)
+                    bufferList.add(buffer)
+                }
+
+                else -> return null
+            }
+        println(bufferList)
+        return bufferList.toTypedArray()
     }
 }
+
+
+//private operator fun String.set(i: Int, value: Char) {
+//
+//}
+//private fun variator (number: String, neighbors:Array<Char>, i: Int, bufferList: MutableList<String>): MutableList<String>{
+//    for (n in 0..neighbors.lastIndex){
+//        val buffer: String = number
+//        buffer[i] = neighbors[n]
+//        bufferList.add(buffer)}
+//    return bufferList
+//}
